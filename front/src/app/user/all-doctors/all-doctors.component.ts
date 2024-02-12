4e8233ffd7da3b549e45e4aedfac7259d2c8b211
hamza halawa <69251476+hamza-halawa2020@users.noreply.github.com>
2024-02-12 11:32:21 +0200

    assad

--- front/src/app/user/all-doctors/all-doctors.component.ts
+++ front/src/app/user/all-doctors/all-doctors.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DoctorService } from '../services/doctor/doctor.service';
-
+import { normalize } from '@angular/common';
 @Component({
   selector: 'app-all-doctors',
   templateUrl: './all-doctors.component.html',
@@ -8,6 +8,8 @@ import { DoctorService } from '../services/doctor/doctor.service';
 })
 export class AllDoctorsComponent {
   doctors: any;
+  filteredDoctors: any;
+  searchTerm: string = '';
 
   constructor(private doctorService: DoctorService) {}
 
@@ -19,8 +21,8 @@ export class AllDoctorsComponent {
     this.doctorService.getAllDoctors().subscribe(
       (data: any) => {
         this.doctors = Object.values(data)[0];
+        this.filteredDoctors = this.doctors;
         // console.log('doctors',this.doctors);
-        
       },
       (error) => {
         console.log('Error fetching doctors:', error);
@@ -41,5 +43,29 @@ export class AllDoctorsComponent {
     });
     return visitRates[0];
   }
+  filterDoctors() {
+    this.filteredDoctors = this.searchTerm
+      ? this.doctors.filter((doctor: any) =>
+          this.matchesSearchTerm(doctor)
+        )
+      : this.doctors;
+  }
 
+  // Helper function to check if the doctor matches the search term
+  matchesSearchTerm(doctor: any): boolean {
+    const normalizedSearchTerm = normalize(this.searchTerm, 'NFKD').toLowerCase();
+    const normalizedNameEn = normalize(doctor.doctor.name_en, 'NFKD').toLowerCase();
+    const normalizedNameEr = normalize(doctor.doctor.name_er, 'NFKD').toLowerCase();
+    const normalizedGovName = normalize(doctor.doctor.gov_name_en, 'NFKD').toLowerCase();
+    const normalizedId = normalize(doctor.doctor.id.toString(), 'NFKD').toLowerCase();
+    const normalizedClass = normalize(doctor.doctor.class, 'NFKD').toLowerCase();
+
+    return (
+      normalizedNameEn.includes(normalizedSearchTerm) ||
+      normalizedNameEr.includes(normalizedSearchTerm) ||
+      normalizedGovName.includes(normalizedSearchTerm) ||
+      normalizedId.includes(normalizedSearchTerm) ||
+      normalizedClass.includes(normalizedSearchTerm)
+    );
+  }
 }
