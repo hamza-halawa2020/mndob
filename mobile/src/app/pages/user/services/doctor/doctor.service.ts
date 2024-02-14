29d1fad032672cdcfb6b6075ad3884bdc06e758f
hamza halawa <69251476+hamza-halawa2020@users.noreply.github.com>
2024-02-14 16:53:28 +0200

    sdsd

--- mobile/src/app/pages/user/services/doctor/doctor.service.ts
+++ mobile/src/app/pages/user/services/doctor/doctor.service.ts
@@ -1,9 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DoctorService {
+  private apiUrl = environment.backEndUrl;
+  constructor(
+    private http: HttpClient,
+  ) {}
 
-  constructor() { }
+  getAllDoctors() {
+    return this.http.get(`${this.apiUrl}doctors`);
+  }
+  getDoctorById(doctorId: number) {
+    const url = `${this.apiUrl}doctors/${doctorId}`;
+    return this.http.get(url);
+  }
+  addDoctor(doctor: any) {
+    return this.http.post(`${this.apiUrl}doctors`, doctor);
+  }
 }
