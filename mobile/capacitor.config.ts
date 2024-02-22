9b04cbfd1f6746764e4265d07423ba1aa8fe20bc
hamza halawa <69251476+hamza-halawa2020@users.noreply.github.com>
2024-02-22 14:25:00 +0200

    sdfdsf

--- mobile/capacitor.config.ts
+++ mobile/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'mobile',
+  appId: 'rimansanId.app',
+  appName: 'rimansan',
   webDir: 'www',
   server: {
     androidScheme: 'https'
